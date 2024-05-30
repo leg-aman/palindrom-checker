ecceadbf107c088a151d9226c08cba48b4338afc
Leg Aman <126026416+leg-aman@users.noreply.github.com>
2024-05-29 22:12:39 -0400

    Update script.js
    updated the last ispalindrome function

--- js/script.js
+++ js/script.js
@@ -2,17 +2,34 @@ loginForm.addEventListener("submit", (e) => {
     e.preventDefault();
   
     let word = document.getElementById("word").value;
-  
+    let res = document.getElementById("result");
+    const mes = isPalindrome(word)
+   console.log(mes)
+    if(mes){
+        res.innerHTML= `${word} is palindrome!`
+    } else {
+        res.innerHTML = `${word} is not palindrome!`
+    }
+  });
+
+  function isPalindrome(word){
     if (word != "" ) {
         // store the word inside two different arrays
         // array1 is stack
         //   array2 is queue
         const arr = []
         const arr2 = []
-        for(let x in word){
-            arr.push(word[x])
-            arr2.push(word[x])
+        for(let char in word){
+            arr.push(word[char])
+            arr2.push(word[char])
         }
-        
-    } 
-  });
+        console.log(arr)
+         
+        while(arr.length > 0 ){
+            if (arr.pop() !== arr2.shift()){
+                return false
+            }
+        }
+        return true
+    }
+  }
